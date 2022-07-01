2899368d1066460b07d8912455f0f0b6be8600a1
techandrotunes <amanmac@192.168.1.6>
2022-07-01 13:01:46 +0530

    other methods added for customer also

--- src/main/kotlin/com/example/beer_app/services/customer/CustomerServiceImpl.java
+++ src/main/kotlin/com/example/beer_app/services/customer/CustomerServiceImpl.java
@@ -1,14 +1,31 @@
 package com.example.beer_app.services.customer;
 
 import com.example.beer_app.web.data.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
     public CustomerDto getCustomerById(UUID customerId) {
         return CustomerDto.builder().customerId(customerId).customerName("Aman").build();
+    }
+
+    @Override
+    public CustomerDto saveCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder().customerId(customerDto.getCustomerId()).customerName(customerDto.getCustomerName()).build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        CustomerDto.builder().customerId(customerId).customerName(customerDto.getCustomerName());
+    }
+
+    @Override
+    public void deleteCustomer(UUID customerId) {
+        log.debug("Delete user customer id-- "+ customerId);
     }
 }
--- src/main/kotlin/com/example/beer_app/web/controller/CustomerController.java
+++ src/main/kotlin/com/example/beer_app/web/controller/CustomerController.java
@@ -3,12 +3,10 @@ package com.example.beer_app.web.controller;
 
 import com.example.beer_app.services.customer.CustomerService;
 import com.example.beer_app.web.data.CustomerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -26,4 +24,25 @@ public class CustomerController {
     ResponseEntity<CustomerDto> getCustomerById(@PathVariable("customerId") UUID customerId){
         return  new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
     }
+
+    @PostMapping
+    ResponseEntity saveCustomer(@RequestBody CustomerDto customerDto){
+        CustomerDto savedCustomer=customerService.saveCustomer(customerDto);
+        HttpHeaders httpHeaders=new HttpHeaders();
+        httpHeaders.add("location",savedCustomer.getCustomerId().toString());
+        return  new ResponseEntity(httpHeaders,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void updateCustomer(@PathVariable("customerId") UUID customerId,@RequestBody CustomerDto customerDto ){
+        customerService.updateCustomer(customerId,customerDto);
+    }
+
+    @DeleteMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteCustomer(@PathVariable("customerId") UUID customerId ){
+        customerService.deleteCustomer(customerId);
+    }
+
 }
